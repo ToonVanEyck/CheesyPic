4f533ea74240eafa933b9aceee8f548db521d960
Toon Van Eyck <toonvaneyck@gmail.com>
2020-06-08 22:01:08 +0200

    added svg rendering, but not implemented dynamically yet.

--- parse_svg.c
+++ parse_svg.c
@@ -8,10 +8,11 @@ void print_photo_location(photo_location_t *photo_location)
     printf("y      : %f\n",photo_location->y);
     printf("width  : %f\n",photo_location->width);
     printf("height : %f\n",photo_location->height);
-    printf("trans  : %s\n",photo_location->transformation_string);
+    for(int i = 0;photo_location->transformation_string[i] != NULL;i++){
+        printf("trans  : %s\n",photo_location->transformation_string[i]);
+    }
     printf("-----------------------------------------------------------------------------------\n");
 }
-
 int xml_node_contains_child_with_name(xmlNode * a_node, const char *name)
 {
     for (xmlNode *child_node = a_node->children; child_node; child_node = child_node->next) {
@@ -21,7 +22,6 @@ int xml_node_contains_child_with_name(xmlNode * a_node, const char *name)
     }
     return 0;
 }
-
 int cnt_photos(xmlNode * a_node)
 {
     int cnt = 0;
@@ -33,9 +33,6 @@ int cnt_photos(xmlNode * a_node)
     }
     return cnt;
 }
-
-
-
 void append_photo_node_to_list(xmlNode * a_node,xmlNode ***photo_nodes, int *index)
 {
     for (xmlNode *cur_node = a_node; cur_node; cur_node = cur_node->next) {
@@ -45,7 +42,6 @@ void append_photo_node_to_list(xmlNode * a_node,xmlNode ***photo_nodes, int *ind
         append_photo_node_to_list(cur_node->children,photo_nodes,index);
     }
 }
-
 void get_photos(xmlNode * a_node, xmlNode ***photo_nodes, int *num_photos)
 {
     *num_photos = cnt_photos(a_node);
@@ -53,7 +49,6 @@ void get_photos(xmlNode * a_node, xmlNode ***photo_nodes, int *num_photos)
     int i = 0;
     append_photo_node_to_list(a_node,photo_nodes,&i);
 }
-
 xmlNode *xml_sibling_node_by_name(xmlNode * a_node, const char *name)
 {
     xmlNode *cur_node = NULL;
@@ -64,7 +59,6 @@ xmlNode *xml_sibling_node_by_name(xmlNode * a_node, const char *name)
     }
     return NULL;
 }
-
 xmlNode *xml_child_node_by_name(xmlNode * a_node, const char *name)
 {
     xmlNode *cur_node = NULL;
@@ -75,7 +69,6 @@ xmlNode *xml_child_node_by_name(xmlNode * a_node, const char *name)
     }
     return NULL;
 }
-
 xmlNode *xml_get_node_where_attribute_has_value(xmlNode * a_node, const char *attribute_key, const char *attribute_value)
 {
     for (xmlNode *child_node = a_node->children; child_node; child_node = child_node->next) {
@@ -89,12 +82,10 @@ xmlNode *xml_get_node_where_attribute_has_value(xmlNode * a_node, const char *at
     return NULL;
 }
 
-
-int main(int argc, char *argv[])
+int get_photo_locations_from_design(const char * svg_file,photo_locations_t *photo_locations)
 {
     xmlParserCtxtPtr ctxt; /* the parser context */
     xmlDocPtr doc; /* the resulting document tree */
-    const char *filename = argv[1];
 
     /* create a parser context */
     ctxt = xmlNewParserCtxt();
@@ -103,39 +94,71 @@ int main(int argc, char *argv[])
 	return 1;
     }
     /* parse the file, activating the DTD validation option */
-    doc = xmlReadFile(filename, NULL, 0);
+    doc = xmlReadFile(svg_file, NULL, 0);
     /* check if parsing suceeded */
-    if(doc == NULL)return -1;
+    if(doc == NULL){
+        xmlFreeDoc(doc);
+        return -1;
+    }
     xmlNode *root_element = xmlDocGetRootElement(doc);
-    if(root_element == NULL)return -1;
+    if(root_element == NULL){
+        xmlFreeDoc(doc);
+        return -1;
+    }
     xmlNode *svg_element = xml_sibling_node_by_name(root_element,"svg");
-    if(svg_element == NULL)return -1;
+    if(svg_element == NULL){
+        xmlFreeDoc(doc);
+        return -1;
+    }
     xmlNode *photos_node = xml_get_node_where_attribute_has_value(svg_element,"label","photos");
-    if(photos_node == NULL)return -1;
+    if(photos_node == NULL){
+        xmlFreeDoc(doc);
+        return -1;
+    }
    
     xmlNode **photo_nodes;
-    int num_photos;
-    get_photos(photos_node, &photo_nodes, &num_photos);
-    photo_location_t *photo_locations = malloc(num_photos * sizeof(photo_location_t));
-    memset(photo_locations,0,num_photos * sizeof(photo_location_t));
-    for(int i = 0; i<num_photos ; i++){
+    get_photos(photos_node, &photo_nodes, &photo_locations->total_photos);
+    photo_locations->photo_location = malloc(photo_locations->total_photos * sizeof(photo_location_t));
+    memset(photo_locations->photo_location,0,photo_locations->total_photos * sizeof(photo_location_t));
+    for(int i = 0; i<photo_locations->total_photos ; i++){
         xmlNode *text_node = xml_child_node_by_name(photo_nodes[i],"text");
         xmlNode *rect_node = xml_child_node_by_name(photo_nodes[i],"rect");
-        photo_locations[i].id = strtol(text_node->children->children->content,NULL,10);
-        photo_locations[i].x = strtod(xmlGetProp(rect_node,"x"),NULL);
-        photo_locations[i].y = strtod(xmlGetProp(rect_node,"y"),NULL);
-        photo_locations[i].width  = strtod(xmlGetProp(rect_node,"width"),NULL);
-        photo_locations[i].height = strtod(xmlGetProp(rect_node,"height"),NULL);
+        photo_locations->photo_location[i].id = strtol(text_node->children->children->content,NULL,10);
+        photo_locations->photo_location[i].x = strtod(xmlGetProp(rect_node,"x"),NULL);
+        photo_locations->photo_location[i].y = strtod(xmlGetProp(rect_node,"y"),NULL);
+        photo_locations->photo_location[i].width  = strtod(xmlGetProp(rect_node,"width"),NULL);
+        photo_locations->photo_location[i].height = strtod(xmlGetProp(rect_node,"height"),NULL);
+        int transform_i = 0;
         for(xmlNode *cur_node = rect_node ; cur_node != svg_element ; cur_node = cur_node->parent){
             char *transform = xmlGetProp(cur_node,"transform");
             if(transform){
-                strcat(photo_locations[i].transformation_string,transform);
-                strcat(photo_locations[i].transformation_string,";");
+                if(strlen(transform)+1<256){
+                    photo_locations->photo_location[i].transformation_string[transform_i] = malloc(strlen(transform)+1);
+                    strcpy(photo_locations->photo_location[i].transformation_string[transform_i],transform);
+                    if(++transform_i>=MAX_TRANSFORMS){
+                        fprintf(stderr,"Error to many transforms!\n");
+                        return -1; 
+                    }
+                }else{
+                    fprintf(stderr,"Error transformation string to long!\n");
+                    return -1;
+                }
             }
         }
-        print_photo_location(&photo_locations[i]);
+        print_photo_location(&photo_locations->photo_location[i]);
     }
-
 	xmlFreeDoc(doc);
-    return 0;
+    return photo_locations->total_photos;
+}
+
+void free_photo_locations(photo_locations_t *photo_locations)
+{
+
+    for(int i = 0;i<photo_locations->total_photos;i++)
+    {
+        for(int j =0;photo_locations->photo_location[i].transformation_string[j] != NULL ; j++){
+            free(photo_locations->photo_location[i].transformation_string[j]);
+        }
+    }
+    free(photo_locations->photo_location);
 }
--- svg_to_png.c
+++ svg_to_png.c
@@ -1,7 +1,135 @@
 #include <librsvg-2.0/librsvg/rsvg.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+//#include <glib/gprintf.h>
+
+#include "parse_svg.h"
+#include "shared_memory.h"
+#include "lodepng.h"
 
 
-int main(int argc, char *argv[])
+#define SVGFILE "../2x6 Ruben Dani.svg"
+
+int load_png_image(const char *file, unsigned *width, unsigned *height, unsigned char **data)
 {
-    printf("fix warnings plis\n");
+    unsigned error = 0;
+
+    unsigned char* png = 0;
+    size_t pngsize;
+
+    error = lodepng_load_file(&png, &pngsize, file);
+    if(!error) error = lodepng_decode32(data, width, height, png, pngsize);
+    if(error) printf("error %u: %s\n", error, lodepng_error_text(error));
+    free(png);
+    return error;
+}
+
+
+int main()
+{
+    photo_locations_t photo_locations;
+    get_photo_locations_from_design(SVGFILE,&photo_locations);
+
+    GError *error = NULL;
+    RsvgHandle *handle;
+
+    RsvgDimensionData dim;
+    double width, height;
+    cairo_surface_t *design_surface, *photo_surface;
+    cairo_t *design_ctx, *photo_ctx;
+    cairo_status_t status;
+    int mode = 1;
+    char * memblock;
+    size_t size;
+
+    unsigned int img_width,img_height;
+    unsigned char *img_data;
+    load_png_image("../logo_whitebg.png",&img_width,&img_height,&img_data);
+    printf("phot %dx%d\n",img_width,img_height);
+    rsvg_set_default_dpi (300.0);
+    handle = rsvg_handle_new_from_file (SVGFILE,&error);
+    rsvg_handle_get_dimensions (handle, &dim);
+    width = dim.width;
+    height = dim.height;
+
+    printf("svg is %fx%f\n",width,height);
+    photo_surface = cairo_image_surface_create_for_data (img_data,CAIRO_FORMAT_RGB24,img_width,img_height,img_width*4);
+    photo_ctx = cairo_create(photo_surface);
+    design_surface = cairo_image_surface_create (CAIRO_FORMAT_RGB24, width, height);
+    design_ctx = cairo_create(design_surface);
+    rsvg_handle_render_cairo(handle, design_ctx);
+
+    status = cairo_status (design_ctx);
+    if (status)
+    {
+        printf("cairo_status!\n");
+        printf ("%s\n",cairo_status_to_string (status));
+        return 1;
+    }
+
+    cairo_matrix_t curr_matrix,result_matrix,transform_matrix;
+    cairo_matrix_init_identity (&curr_matrix);
+    cairo_matrix_init_identity (&transform_matrix);
+
+    cairo_matrix_translate(&curr_matrix,50.499943,-25.000265);
+    cairo_matrix_scale(&curr_matrix,608.0/(double)img_width,456.0/(double)img_height);
+
+    transform_matrix.xx = -0.89970838;
+    transform_matrix.yx = 0.18243401;
+    transform_matrix.xy = -0.18327944;
+    transform_matrix.yy = -0.86352774;
+    transform_matrix.x0 = 1355.1498;
+    transform_matrix.y0 = 1029.9464;
+
+    cairo_matrix_multiply (&result_matrix,&curr_matrix,&transform_matrix);
+    memcpy(&curr_matrix,&result_matrix,sizeof(cairo_matrix_t));
+    cairo_matrix_init_identity(&transform_matrix);
+
+
+    cairo_matrix_translate(&transform_matrix,708.76158,980.34695);
+    cairo_matrix_rotate(&transform_matrix,177.3573*(G_PI/180));
+    cairo_matrix_translate(&transform_matrix,-708.76158,-980.34695);
+
+    cairo_matrix_multiply (&result_matrix,&curr_matrix,&transform_matrix);
+    memcpy(&curr_matrix,&result_matrix,sizeof(cairo_matrix_t));
+    cairo_matrix_init_identity(&transform_matrix);
+
+        printf("matrix(%f,%f,%f,%f,%f,%f)\n",
+        curr_matrix.xx,
+        curr_matrix.yx,
+        curr_matrix.xy,
+        curr_matrix.yy,
+        curr_matrix.x0,
+        curr_matrix.y0);
+
+
+    cairo_set_matrix(design_ctx,&curr_matrix);
+    cairo_set_source_surface(design_ctx,photo_surface,00,00);
+    cairo_paint(design_ctx);
+
+    // unsigned char * cairo_image_surface_get_data (cairo_surface_t *surface);
+    cairo_surface_write_to_png (design_surface, "print_me.png");
+
+    // int raw_width = cairo_image_surface_get_width (surface);
+    // int raw_height = cairo_image_surface_get_height (surface);
+    // int row_byte_size = cairo_image_surface_get_stride (surface);
+    // printf("\nWIDTH: %d, HEIGHT: %d, row_bytes=%d\n", raw_width, raw_height, row_byte_size);
+
+    // unsigned char * raw_buffer = cairo_image_surface_get_data(surface);
+
+    // FILE * pFile;
+    // pFile = fopen ("myfile.bin", "wb");
+    // fwrite (raw_buffer , sizeof(char), raw_height*row_byte_size, pFile);
+    // fclose (pFile);
+
+    cairo_destroy (design_ctx);
+    cairo_surface_destroy (design_surface);
+
+    cairo_destroy (photo_ctx);
+    cairo_surface_destroy (photo_surface);
+
+    free_photo_locations(&photo_locations);
+
+    return 0;
 }
